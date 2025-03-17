b0ee56fdaa553965824efd2f7d0d4d9a8f325749
MarmolejoValle <20223tn132@utez.edu.mx>
2025-03-16 21:18:19 -0600

    start project

---
+++ communityCommitteeSystem/src/test/java/utez/edu/mx/communitycommitteesystem/CommunityCommitteeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.communitycommitteesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityCommitteeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
