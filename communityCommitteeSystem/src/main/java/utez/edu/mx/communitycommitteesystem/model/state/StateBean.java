d77c99feb13db30300f5175458b3c7cdb1f5c40e
LizSan7 <20223tn031@utez.edu.mx>
2025-03-16 22:31:12 -0600

    add model state

---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/model/state/StateBean.java
@@ -0,0 +1,24 @@
+package utez.edu.mx.communitycommitteesystem.model.state;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import utez.edu.mx.communitycommitteesystem.model.person.PersonBean;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "state")
+public class StateBean {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String nameState;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "idPerson")
+    private PersonBean personBean;
+}
