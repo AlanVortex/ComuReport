be645e62b887fd7613bf2db950f3f88232846922
MarmolejoValle <20223tn132@utez.edu.mx>
2025-03-16 22:54:22 -0600

    add model municipality

---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/model/municipality/MunicipalityBean.java
@@ -0,0 +1,26 @@
+package utez.edu.mx.communitycommitteesystem.model.municipality;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import utez.edu.mx.communitycommitteesystem.model.person.PersonBean;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "municipality")
+public class MunicipalityBean {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String nameMunicipality;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "idPerson")
+    private PersonBean personBean;
+
+
+}
