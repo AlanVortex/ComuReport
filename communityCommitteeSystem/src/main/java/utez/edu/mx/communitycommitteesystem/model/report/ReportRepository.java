ce2a2e008271a60901a2d27318882cf0a6112d2b
LizSan7 <20223tn031@utez.edu.mx>
2025-03-17 21:47:25 -0600

    add report model

---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/model/report/ReportRepository.java
@@ -0,0 +1,6 @@
+package utez.edu.mx.communitycommitteesystem.model.report;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<ReportBean, Long> {
+}
