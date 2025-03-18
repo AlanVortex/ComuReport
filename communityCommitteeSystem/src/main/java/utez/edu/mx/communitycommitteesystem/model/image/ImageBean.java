a6e05295ec3a9dfc2c2f9b9d0b3c3082bcdb48f0
LizSan7 <20223tn031@utez.edu.mx>
2025-03-17 21:49:22 -0600

    add area and report model

---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/model/image/ImageBean.java
@@ -0,0 +1,24 @@
+package utez.edu.mx.communitycommitteesystem.model.image;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import utez.edu.mx.communitycommitteesystem.model.report.ReportBean;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "image")
+public class ImageBean {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idReport")
+    private ReportBean reportBean;
+}
