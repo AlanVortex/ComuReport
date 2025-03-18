50d5005892c84816af562fbee4ff622f1d59cb5c
LizSan7 <20223tn031@utez.edu.mx>
2025-03-17 21:54:39 -0600

    add status model

---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/model/status/StatusRepository.java
@@ -0,0 +1,6 @@
+package utez.edu.mx.communitycommitteesystem.model.status;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatusRepository extends JpaRepository<StatusBean, Long> {
+}
