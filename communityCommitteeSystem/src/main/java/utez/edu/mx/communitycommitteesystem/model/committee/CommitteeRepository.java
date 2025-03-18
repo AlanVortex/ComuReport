0b56330a68c5aad90c21649ed9ef05d27e63d4f2
LizSan7 <20223tn031@utez.edu.mx>
2025-03-17 21:53:08 -0600

    add committee model

---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/model/committee/CommitteeRepository.java
@@ -0,0 +1,6 @@
+package utez.edu.mx.communitycommitteesystem.model.committee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommitteeRepository extends JpaRepository<CommitteeBean, Long> {
+}
