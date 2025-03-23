2761775f8806b1e11343f24808209ae6628133ff
LizSan7 <20223tn031@utez.edu.mx>
2025-03-23 00:15:20 -0600

    adding ColonyController and ColonyWithLinkDto

---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/controller/colony/ColonyController.java
@@ -0,0 +1,55 @@
+package utez.edu.mx.communitycommitteesystem.controller.colony;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import utez.edu.mx.communitycommitteesystem.model.colony.ColonyBean;
+import utez.edu.mx.communitycommitteesystem.model.municipality.MunicipalityBean;
+import utez.edu.mx.communitycommitteesystem.model.person.PersonBean;
+import utez.edu.mx.communitycommitteesystem.service.colony.ColonyService;
+import utez.edu.mx.communitycommitteesystem.service.municipality.MunicipalityService;
+import utez.edu.mx.communitycommitteesystem.service.person.PersonService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/colony")
+@CrossOrigin(origins = "*")
+public class ColonyController {
+
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private MunicipalityService municipalityService;
+
+    @Autowired
+    private ColonyService colonyService;
+
+    @PostMapping("/register-colonyWithLink")
+    public ResponseEntity<String> registerColonyWithLink(@RequestBody ColonyWithLinkDto dto) {
+        PersonBean person = new PersonBean();
+        person.setName(dto.getName());
+        person.setLastname(dto.getLastname());
+        person.setEmail(dto.getEmail());
+        person.setPassword(dto.getPassword());
+        person.setPhone(dto.getPhone());
+
+        PersonBean savedPerson = personService.saveColony(person);
+
+        Optional<MunicipalityBean> municipalityOpt = municipalityService.findById(dto.getMunicipalityId());
+        if (!municipalityOpt.isPresent()) {
+            return ResponseEntity.badRequest().body("Municipio no encontrado");
+        }
+
+        ColonyBean colony = new ColonyBean();
+        colony.setNameColony(dto.getColonyName());
+        colony.setMunicipalityBean(municipalityOpt.get());
+        colony.setPersonBean(savedPerson);
+
+        colonyService.save(colony);
+
+        return ResponseEntity.ok("Colonia y enlace colonial registrados correctamente");
+    }
+
+}
---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/controller/colony/ColonyWithLinkDto.java
@@ -0,0 +1,73 @@
+package utez.edu.mx.communitycommitteesystem.controller.colony;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ColonyWithLinkDto {
+
+    private String colonyName;
+    private Long municipalityId;
+    private String name;
+    private String lastname;
+    private String email;
+    private String password;
+    private String phone;
+
+    public String getColonyName() {
+        return colonyName;
+    }
+
+    public void setColonyName(String colonyName) {
+        this.colonyName = colonyName;
+    }
+
+    public Long getMunicipalityId() {
+        return municipalityId;
+    }
+
+    public void setMunicipalityId(Long municipalityId) {
+        this.municipalityId = municipalityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
