c01985307dcab32f25157f367573bdc1812d5e83
LizSan7 <20223tn031@utez.edu.mx>
2025-03-22 19:05:45 -0600

    add findbyemail and findbyid

--- communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/model/person/PersonRepository.java
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/model/person/PersonRepository.java
@@ -3,4 +3,6 @@ package utez.edu.mx.communitycommitteesystem.model.person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<PersonBean,Long> {
+    PersonBean findByEmail(String email);
+
 }
