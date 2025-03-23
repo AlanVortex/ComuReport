04a079d486783367fdbb15910ba1e8581b05b636
LizSan7 <20223tn031@utez.edu.mx>
2025-03-22 23:29:14 -0600

    adding AssingnAdminMunDto and StateService

---
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/service/state/StateService.java
@@ -0,0 +1,18 @@
+package utez.edu.mx.communitycommitteesystem.service.state;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utez.edu.mx.communitycommitteesystem.model.state.StateBean;
+import utez.edu.mx.communitycommitteesystem.model.state.StateRepository;
+
+import java.util.Optional;
+
+@Service
+public class StateService {
+    @Autowired
+    private StateRepository stateRepository;
+
+    public Optional<StateBean> findById(Long id) {
+        return stateRepository.findById(id);
+    }
+}
