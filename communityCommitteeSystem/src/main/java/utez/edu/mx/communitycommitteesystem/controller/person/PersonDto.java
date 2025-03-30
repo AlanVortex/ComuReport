48b700084f55860f4c8073c17b46f7bc0d117ad3
MarmolejoValle <20223tn132@utez.edu.mx>
2025-03-30 16:02:42 -0600

    method getPersonBean

--- communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/controller/person/PersonDto.java
+++ communityCommitteeSystem/src/main/java/utez/edu/mx/communitycommitteesystem/controller/person/PersonDto.java
@@ -3,6 +3,8 @@ package utez.edu.mx.communitycommitteesystem.controller.person;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import utez.edu.mx.communitycommitteesystem.model.person.PersonBean;
 
 @NoArgsConstructor
 @Getter
@@ -13,4 +15,16 @@ public class PersonDto {
     private String email;
     private String password;
     private String phone;
+
+    public PersonBean getPersonBean(){
+        PersonBean personBean = new PersonBean();
+        personBean.setName(name);
+        personBean.setLastname(lastname);
+        personBean.setEmail(email);
+        BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+        String encryptedPsw = bcrypt.encode(password);
+        personBean.setPassword(encryptedPsw);
+        personBean.setPhone(phone);
+        return personBean;
+    }
 }
